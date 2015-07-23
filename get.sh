5e079d1cad873034b7e7c2a643f9449ab114c6b4
i.vetrov <ig-wind@mail.ru>
2015-07-23 23:13:16 +0300

    Get components to build ffmpeg

---
+++ get.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# get components to build ffmpeg
+
+#wget http://downloads.sourceforge.net/faac/faac-1.28.tar.bz2
+wget http://netcologne.dl.sourceforge.net/project/faac/faac-src/faac-1.28/faac-1.28.tar.bz2
+tar xfj faac-1.28.tar.bz2
+#wget http://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz
+wget http://kent.dl.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+tar xfz lame-3.99.5.tar.gz
+git clone git://git.videolan.org/x264.git
+cd x264 && git checkout stable && cd ..
+wget https://www.libsdl.org/release/SDL-1.2.15.tar.gz
+tar xfz SDL-1.2.15.tar.gz
+wget http://ffmpeg.org/releases/ffmpeg-2.6.3.tar.bz2
+tar xfj ffmpeg-2.6.3.tar.bz2
